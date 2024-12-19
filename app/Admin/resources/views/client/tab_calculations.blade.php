1b6365c117585157c69ec9129b44099e48828aee
xerxes <odilbekovjavohir4@gmail.com>
2024-12-19 20:03:06 +0500

    moved js that comments said wouldn't work

--- app/Admin/resources/views/client/tab_calculations.blade.php
+++ app/Admin/resources/views/client/tab_calculations.blade.php
@@ -156,3 +156,75 @@
 @else
     <h3>@lang('common.formular_data_empty')</h3>
 @endif
+@push('footer-scripts')
+    <script>
+        $('#allow_custom_nutrients').on('change', function () {
+            let allowed = $(this).prop('checked');
+            $('.allowed_custom_nutrients').attr('readonly', !allowed);
+            $('.allowed_custom_nutrients_btn').css('display', (allowed) ? 'inline-block' : 'none');
+        });
+        $('#allow_custom_nutrients').trigger('click');
+
+        $('#calc_auto').on('change', function () {
+            let approve = $(this).prop('checked');
+
+            Swal.fire({
+                title: '{{trans('admin.messages.confirmation')}}',
+                text: '{{trans('admin.messages.revert_warning')}}',
+                icon: 'warning',
+                showCancelButton: true,
+                allowOutsideClick: false,
+                allowEscapeKey: false,
+                allowEnterKey: false,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: '{{ __('admin.filters.defaults.exist') }}',
+                cancelButtonText: '{{ __('admin.filters.defaults.missing') }}',
+            }).then((result) => {
+                if (result.value) {
+                    Swal.fire({
+                        title: '{{trans('admin.messages.wait')}}',
+                        text: '{{trans('admin.messages.in_progress')}}',
+                        allowOutsideClick: false,
+                        allowEscapeKey: false,
+                        allowEnterKey: false,
+                        didOpen: () => {
+                            Swal.showLoading();
+                        },
+                    });
+
+                    $.ajax({
+                        type: 'POST',
+                        url: "{{ route('admin.client.calc-auto') }}",
+                        dataType: 'json',
+                        data: {
+                            _token: $('meta[name=csrf-token]').attr('content'),
+                            userId: clientId,
+                            approve: approve,
+                        },
+                        success: function (data) {
+                            if (data.success === true) {
+                                Swal.hideLoading();
+                                Swal.fire({
+                                    icon: 'success',
+                                    title: '{{trans('admin.messages.saved')}}',
+                                    html: data.message,
+                                });
+                            } else {
+                                Swal.hideLoading();
+                                Swal.fire({
+                                    icon: 'error',
+                                    title: 'Oops...',
+                                    html: data.message,
+                                });
+                            }
+                        },
+                    });
+                } else {
+                    $(this).prop('checked', !approve);
+                }
+            });
+
+        });
+    </script>
+@endpush
--- app/Admin/resources/views/client/tableEntity.blade.php
+++ app/Admin/resources/views/client/tableEntity.blade.php
@@ -190,5 +190,113 @@
         @endif
         localStorage.removeItem(selectedPopupRecipesStorage);
         localStorage.removeItem(selectedPopupRecipesStorage);
+        $tableUsers = $('#table-users')
+            .DataTable({
+                searching: false,
+                lengthChange: false,
+                processing: true,
+                serverSide: true,
+                deferRender: true,
+                pageLength: 20,
+                paging: {
+                    type: 'input',
+                    buttons: 10,
+                },
+                select: {
+                    style: 'multi',
+                    selector: 'td:not(:last-child)',
+                },
+                order: [[0, 'desc']],
+                ajax: {
+                    url: "{{ route('admin.datatable.async')}}",
+                    data: function (d) {
+                        d.method = 'allUsers';
+                        d.filter = getFormData($('#user-filter'));
+                    },
+                },
+                layout: {
+                    topStart: null,
+                    topEnd: 'info',
+                    bottomStart: 'info',
+                    bottomEnd: 'paging'
+                },
+                columns: [
+                    {orderable: true, data: 'id'},
+                    {data: 'first_name'},
+                    {data: 'last_name'},
+                    {data: 'email'},
+                    {data: 'formular_approved', orderable: false},
+                    {data: 'subscription', orderable: false},
+                    {data: 'status', orderable: false},
+                    {data: 'lang'},
+                    {data: 'created_at'},
+                    {
+                        data: null,
+                        className: 'text-center',
+                        orderable: false,
+                        width: '80px',
+                        render: function (data, type, row) {
+                            return '<a href="{{ url('/') }}/admin/users/' + row.id +
+                                '/edit" class="btn btn-xs btn-primary" title="Edit" data-toggle="tooltip">' +
+                                '<span class="fas fa-pencil-alt"></span>' +
+                                '</a>'
+                                    @can(PermissionEnum::DELETE_CLIENT->value, '\App\Models\Admin')
+                                + ' ' +
+                                '<form action="{{ url('/') }}/admin/users/' + row.id +
+                                '/delete" method="POST" style="display:inline-block;">' +
+                                '{{ csrf_field() }}' +
+                                '<input type="hidden" name="_method" value="delete">' +
+                                '<button class="btn btn-xs btn-danger btn-delete" title="Delete" data-toggle="tooltip">' +
+                                '<i class="fas fa-trash-alt"></i>' +
+                                '</button>' +
+                                '</form>';
+                            @endcan
+                        },
+                    },
+                ],
+            })
+            .on('draw', function (e, settings) {
+                $tableUsers.rows().every(function () {
+                    let rowData = this.data();
+                    let alreadySelected = JSON.parse(localStorage.getItem(selectedUsersStorage));
+                    if (alreadySelected === null || alreadySelected.selected.length === 0) {
+                        return;
+                    }
+                    if (alreadySelected.selected.includes(rowData.id)) {
+                        this.select();
+                    }
+                });
+            })
+            .on('select', function (e, dt, type, indexes) {
+                let rowData = $tableUsers.rows(indexes).data();
+                let alreadySelected = JSON.parse(localStorage.getItem(selectedUsersStorage))
+                if (alreadySelected === null) {
+                    alreadySelected = {'selected': []};
+                }
+
+                // serialize selected rows
+                $.each(rowData, function (index, row) {
+                    if (!alreadySelected.selected.includes(row.id)) {
+                        alreadySelected.selected.push(row.id);
+                    }
+                });
+                localStorage.setItem(selectedUsersStorage, JSON.stringify(alreadySelected));
+            })
+            .on('deselect', function (e, dt, type, indexes) {
+                let rowData = $tableUsers.rows(indexes).data();
+                let alreadySelected = JSON.parse(localStorage.getItem(selectedUsersStorage))
+                if (alreadySelected === null) {
+                    return;
+                }
+                $.each(rowData, function (index, row) {
+                    let location = alreadySelected.selected.indexOf(row.id);
+
+                    if (location !== -1) {
+                        alreadySelected.selected.splice(location, 1);
+                    }
+                });
+
+                localStorage.setItem(selectedUsersStorage, JSON.stringify(alreadySelected));
+            });
     </script>
 @endpush
